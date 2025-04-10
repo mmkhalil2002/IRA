72a0b6f7923febf6ddd243e1b164d7c68e08a477
root <root@ubuntu.lan>
2025-04-10 00:19:42 +0000

    update init_scripts

--- etc/wifi-eth-config/init_scripts/0-install-header.sh
+++ etc/wifi-eth-config/init_scripts/0-install-header.sh
@@ -1,6 +1,9 @@
 if [ -d "/dev/src" ] && [ -z "$(ls -A /dev/src)" ]; then
-    echo "/dev/src is empty"
-    sudo apt install linux-headers-$(uname -r)
+	echo "/dev/src is empty"
+	cd /dev/src
+	sudo apt install linux-headers-$(uname -r)
+	echo "/usr/src/linux-headers-$path/"
+	sudo ln -s /usr/src/linux-headers-$path/arch/arm64 /usr/src/linux-headers-$path/arch/$(uname -m)
 else
-    echo "/dev/src is not empty"
+	echo "/dev/src is not empty"
 fi
